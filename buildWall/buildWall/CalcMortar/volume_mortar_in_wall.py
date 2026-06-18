6ded7eb6319851ad7d77dcace31ae62dcb776709
Nikita67S <mrsmrsx123@gmail.com>
2026-06-18 12:12:27 +0300

    Add files via upload

---
+++ buildWall/buildWall/CalcMortar/volume_mortar_in_wall.py
@@ -0,0 +1,17 @@
+"""Comments about module"""
+
+
+def mortar_volume(v_wall: float, constant: float) -> float:
+    """
+
+    Args:
+        v_wall:
+        constant:
+
+    Returns:
+
+    """
+    return v_wall * constant
+
+
+
